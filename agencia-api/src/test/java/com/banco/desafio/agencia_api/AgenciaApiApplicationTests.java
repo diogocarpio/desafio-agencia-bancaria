dace8324aa780b95d5f95b053061caad2ef520c8
Diogo Felix <diogo.felix92@hotmail.com>
2025-09-17 11:50:09 -0300

    docs: Criando a estrtura inicial do projeto com suas dependencias

---
+++ agencia-api/src/test/java/com/banco/desafio/agencia_api/AgenciaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.desafio.agencia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
